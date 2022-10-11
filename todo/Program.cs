175017cb15431cbc55231f4c2f30f8dd039ab82a
Olga Torres <bestlaly@mail.ru>
2022-10-11 12:16:27 +0300

    Method added

--- todo/Program.cs
+++ todo/Program.cs
@@ -1,2 +1,20 @@
-﻿Console.WriteLine("Input your list with space");
+﻿string [] ChangeArray(string inputString)
+// Метод для выбора строк не более 2 символов
+{
+    var myArray = inputString.Split(' ');
+
+    var newArray = new string[myArray.Length];
+    var index = 0;
+    foreach (var value in myArray)
+    {
+        if (value.Length < 3)
+        {
+            newArray[index] = value;
+            index++;
+        }
+    }
+    return newArray;
+}
+
+Console.WriteLine("Input your list with space");
 string inputString = Console.ReadLine();
